3f105b4c05af5c687fb8f6e70b455ca5374e5e76
carlosm110 <carlosm110206@gmail.com>
2025-06-25 09:55:30 -0500

    Implementación Login

--- SistemaVentas.MVC/Program.cs
+++ SistemaVentas.MVC/Program.cs
@@ -9,19 +9,27 @@ namespace SistemaVentas.MVC
     {
         public static void Main(string[] args)
         {
-
             Crud<Seat>.EndPoint = "https://localhost:7269/api/Seats";
             Crud<Ticket>.EndPoint = "https://localhost:7269/api/Tickets";
             Crud<Category>.EndPoint = "https://localhost:7269/api/Categories";
             Crud<Customer>.EndPoint = "https://localhost:7269/api/Customers";
             Crud<Model.Route>.EndPoint = "https://localhost:7269/api/Routes";
+            Crud<Admin>.EndPoint = "https://localhost:7269/api/Admins";
 
 
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<SistemaVentasDBContext>(options =>
-
                 options.UseNpgsql(builder.Configuration.GetConnectionString("SistemaVentasDBContext") ?? throw new InvalidOperationException("Connection string 'SistemaVentasDBContext' not found.")));
+
+            // Agregar soporte para sesiones
+            builder.Services.AddDistributedMemoryCache(); // Utiliza la memoria como caché
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // Tiempo de expiración de la sesión
+                options.Cookie.HttpOnly = true; // Evitar que las cookies de sesión se accedan desde el lado del cliente
+                options.Cookie.IsEssential = true; // Hacer que la cookie sea esencial para el funcionamiento de la aplicación
+            });
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -41,11 +49,14 @@ namespace SistemaVentas.MVC
 
             app.UseRouting();
 
+            // Habilitar sesión
+            app.UseSession();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Auth}/{action=Login}/{id?}");
 
             app.Run();
         }
